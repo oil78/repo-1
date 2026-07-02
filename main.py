59ac96666c763efbf7db9ab70c1a460c456b1c6e
oil78 <matt_h78@hotmail.com>
2026-07-02 11:15:09 +0000

    Version change commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World V2'
+    return 'Welcome to Python Flask World V1'
 
 if __name__=='__main__':
      app.run(host='0.0.0.0', port=8080)
